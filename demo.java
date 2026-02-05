7e7f006c2a446e3e830c05eb32db1bd6f337de5e
venkateshpd07 <151019073+venkateshpd07@users.noreply.github.com>
2026-02-05 13:48:37 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,8 +1,8 @@
 class demo {
         public static void main(String[] args){
                 String s="welcme to devops";
-                rev="";
-                fro(int i=s.length()-1;i>=0;i--){
+                String rev="";
+                for(int i=s.length()-1;i>=0;i--){
                         rev+=s.charAt(i);
         }
                 System.out.println(rev);
